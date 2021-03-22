a4b43aa0a92640915347e6f2f7be0c17e254f8f6
TheGubbys <elmevej18denni@live.dk>
2021-03-22 14:20:11 +0100

    lavet start af header

--- index.php
+++ index.php
@@ -19,14 +19,19 @@
 
     <!-- Sikrer man kan benytte CSS ved at tilkoble en CSS fil -->
     <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
 
     <!-- Sikrer den vises korrekt på mobil, tablet mv. ved at tage ift. skærmstørrelse - bliver brugt til responsive websider -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
 <!-- i <body> har man alt indhold på siden som brugeren kan se -->
-<body>
+<body class="container-xl">
 
+<?php include 'header.php' ?>
+
+
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 <!-- Her skal sidens indhold ligge -->
 
 </body>
