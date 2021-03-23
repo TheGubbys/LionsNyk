552a2453d761e1f4d277b55638ef41638fd7ad2e
TheGubbys <elmevej18denni@live.dk>
2021-03-23 16:01:22 +0100

    Forsøgt at ændre tekstfarve på header, uden succes. Bootstrap's basiske navbar-dark virker, men det er ikke del af de farver vi har aftalt at gøre brug af, så er det tid til overs, gå tilbage til dette.
    - Ændret søge knap til ikon, kan ikke fjerne border fra den, så lige nu ser det grimt ud. Samme problem med farver er her, se på det hvis der er tid til overs.

--- header.php
+++ header.php
@@ -2,14 +2,14 @@
 <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 
-<nav class="navbar navbar-expand-lg navbar-light">
+<header class="navbar navbar-expand-lg">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">
             <img src="./images/LionsNykLogo.png" alt="" width="auto" height="64">
         </a>
     </div>
-</nav>
-<nav class="under navbar navbar-expand-lg navbar-light sticky-top">
+</header>
+<nav class="under navbar navbar-expand-lg navbar-dark text-white sticky-top">
     <div class="container-fluid">
         <div class="navbar-brand"></div>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -52,7 +52,10 @@
             </ul>
             <form class="d-flex">
                 <input class="form-control me-2" type="search" placeholder="..." aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Søg</button>
+                <button type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                    </svg>
+                </button>
             </form>
         </div>
     </div>
