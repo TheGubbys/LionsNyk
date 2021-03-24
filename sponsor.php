f4eb529969cde67a120a1cb0980550b5edf553f1
TheGubbys <elmevej18denni@live.dk>
2021-03-24 16:16:06 +0100

    forsøgt at se på sponsor siden, sat den op så det passede ind i den skabelon, men noget fungerer ikke. Springer videre, men gå tilbage hvis der er tid.

--- sponsor.php
+++ sponsor.php
@@ -18,10 +18,9 @@
     <meta name="copyright" content="Information om copyright">
 
     <!-- Sikrer man kan benytte CSS ved at tilkoble en CSS fil -->
-    <link href="css/styles.css" rel="stylesheet" type="text/css">
     <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
     <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
 
     <!-- Sikrer den vises korrekt på mobil, tablet mv. ved at tage ift. skærmstørrelse - bliver brugt til responsive websider -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -32,63 +31,37 @@
 
 <!-- Her skal sidens indhold ligge -->
 <?php include 'header.php'; ?>
-<div class="container">
-    <!-- Content here -->
-    <div class="row marg">
-        <img src="images/main.PNG" alt="main">
-    </div>
 
-
-
-
-
-    <div class="row">
-        <div class="col-12 col-md-6 col-xl-6">
-           <div class="">
-               <p class="marg text">Som sponsor kan du selv bestemme hvor meget du vil donere til klubben,
-                   der vil ikke ses forskelligt på sponsorerne udfra hvor meget de hjælper til med donationer.
-                   Alt er sat pris på og betyder meget for os. </p>
-
-           </div>
+<main role="main" class="container-fluid">
+        <div class="row marg">
+            <img src="images/main.PNG" alt="main">
         </div>
-
-        <div class="col-12 col-md-6 col-xl-6">
-            <img class="marg" src="images/sponsor.PNG" alt="sponsor">
-        </div>
-    </div>
-
-
-
-    <div class="row">
-
-        <div class="col-2">
-            <div>
-                <button type="button" class="btn btn-warning">Doner</button>
+        <div class="row">
+            <div class="col-12 col-md-6 col-xl-6">
+                <div class="">
+                    <p class="marg text">Som sponsor kan du selv bestemme hvor meget du vil donere til klubben,
+                        der vil ikke ses forskelligt på sponsorerne udfra hvor meget de hjælper til med donationer.
+                        Alt er sat pris på og betyder meget for os. </p>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-xl-6">
+                <img class="marg" src="images/sponsor.PNG" alt="sponsor">
             </div>
         </div>
-
-
-
-
-        <div class="col-2">
-            <div>
-                <button type="button" class="btn btn-success">Læsemere</button>
+        <div class="row">
+            <div class="col-2">
+                <div>
+                    <button type="button" class="btn btn-warning">Doner</button>
+                </div>
+            </div>
+            <div class="col-2">
+                <div>
+                    <button type="button" class="btn btn-success">Læsemere</button>
+                </div>
             </div>
         </div>
-
-
-
-
-
     </div>
-
-
-
-
-
-</div>
-
-<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</main>
 
 <?php include 'footer.php'; ?>
 
