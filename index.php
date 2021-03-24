c5cc230ae45cd04bef0130a51b783fd71ffdf323
TheGubbys <elmevej18denni@live.dk>
2021-03-24 21:19:04 +0100

    Lavet forside, herunder carousel, og call to action knapper.

--- index.php
+++ index.php
@@ -20,6 +20,7 @@
     <!-- Sikrer man kan benytte CSS ved at tilkoble en CSS fil -->
     <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
     <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="css/memberstyles.css" rel="stylesheet" type="text/css">
     <link href="css/styles.css" rel="stylesheet" type="text/css">
     <!-- Sikrer den vises korrekt på mobil, tablet mv. ved at tage ift. skærmstørrelse - bliver brugt til responsive websider -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -30,9 +31,54 @@
 
 <?php include 'header.php'; ?>
 
-<main role="main" class="container-fluid">
+<main role="main" class="container-fluid w-75">
     <!-- Her skal sidens indhold ligge -->
-
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="images/slideshow1.jpg" class="d-block w-100" alt="slideshow images 1">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>TOGETHER</h5>
+                    <p>WE SERVE</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="images/slideshow2.jpg" class="d-block w-100" alt="slideshow images 2">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>TOGETHER</h5>
+                    <p>WE HELP</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="images/slideshow3.jpg" class="d-block w-100" alt="slideshow image 3">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>TOGETHER</h5>
+                    <p>WE LIVE</p>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+    <div class="text">
+        <h1>Vi er Lions Nykøbing</h1>
+        <p>Ønsker du at lære mere om hvem vi er, så klik her</p>
+        <button type="button" class="btn btn-info" href="#OmOs">Om Klubben</button>
+        <h1>Bliv Medlem</h1>
+        <p>Ønsker du at blive medlem, og støtte Lions samt det vi står for, så klik her.</p>
+        <button type="button" class="btn btn-info" href="#BlivMedlem">Bliv Medlem</button>
+    </div>
 </main>
 
 <?php include 'footer.php'; ?>
